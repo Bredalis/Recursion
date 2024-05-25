b4d0542453afa292c7b409293e033e8a62e07f8d
PRO X <bredalisgautreaux@gmail>
2024-05-25 10:36:34 +0200

    Programas de recursion simple

---
+++ Busqueda_Binaria.py
@@ -0,0 +1,49 @@
+
+# Busqueda del dato
+# Manera Iterativa
+
+lista = [11, 12, 13, 14, 15, 16, 17]
+
+def busqueda_iterativa(lista, dato):
+
+	bajo = 0
+	alto = len(lista) - 1
+	centro = (bajo + alto) // 2
+
+	while bajo <= alto:
+
+		if lista[centro] == dato:
+			return centro
+
+		elif lista[centro] < dato:
+			bajo = centro + 1
+
+		else:
+			alto = centro - 1
+
+		centro = (bajo + alto) // 2
+
+	return -1
+
+# Manera Recursiva
+
+def busqueda_recursiva(lista, bajo, alto, dato):
+
+	if bajo > alto:
+		return -1
+
+	centro = (bajo + alto) // 2
+
+	if lista[centro] == dato:
+		return centro
+
+	elif lista[centro] < dato:
+		return busqueda_recursiva(lista, centro + 1, alto, dato)
+
+	else:
+		return busqueda_recursiva(lista, bajo, centro - 1, dato)
+
+for i in range(10, 19):
+
+	print(f"Iterativa: {i} _ {busqueda_iterativa(lista, i)}")
+	print(f"Recursiva: {i} _ {busqueda_recursiva(lista, 0, len(lista) - 1, i)}")
---
+++ Funciones.py
@@ -0,0 +1,65 @@
+"""
+Funciones Recursivas: son funciones 
+que se llaman a sí mismas para resolver 
+un problema dividiéndolo en instancias más
+pequeñas del mismo problema.
+
+Funciones Iterativas: son funciones que 
+utilizan bucles o ciclos para ejecutar 
+un conjunto de instrucciones repetidamente 
+hasta que se cumple una condición de salida.
+
+Factorizacion: es el proceso matemático de 
+descomponer una expresión o número en factores 
+más simples que, al multiplicarse juntos, 
+producen el valor original.
+"""
+
+from time import perf_counter
+
+# Funciones Recursivas
+
+def recursiva(numero):
+	print("Recursiva:\n")
+
+	if numero == 0:
+		print(numero)
+
+	else:
+		print(numero)
+		recursiva(numero - 1)
+
+def factorizacion_recursiva(numero):
+	print("\nFactorizacion recursiva: \n")
+
+	if numero == 0 or numero == 1:
+		return 1
+
+	return numero * factorizacion_recursiva(numero - 1)
+
+# Funcion Iterativa
+
+def factorizacion_iteractiva(numero):
+	print("\nFactorizacion Iteractiva: \n")
+
+	if numero == 0 or numero == 1:
+		print(1)
+
+	resultado = 1
+
+	for i in range(2, numero + 1):		
+		resultado *= i
+
+	return resultado
+
+inicio = perf_counter()
+
+for i in range(10):
+	print(i, factorizacion_iteractiva(i), factorizacion_recursiva(i))
+
+for i in range(10):
+	recursiva(i)
+
+final = perf_counter()
+
+print("Tiempo:", "%0.4f" % (final - inicio))
